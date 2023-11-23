9dd498257228cf30129ba7145b2094008f7d63eb
Bowie <andersoncruz2908@gmail.com>
2023-11-23 11:16:28 -0300

    Primeiro commit

---
+++ src/poo/Numero.java
@@ -0,0 +1,21 @@
+package poo;
+
+public class Numero {
+  private double x;
+
+  private double getX() {
+    return x;
+  }
+
+  private void setX(double x) {
+    this.x = x;
+  }
+
+  public double soma(double x, double y) {
+  
+  setx(x + y);
+  return getX();
+  }
+
+
+}
---
+++ src/poo/TestaNumero.java
@@ -0,0 +1,23 @@
+package poo;
+
+import java.util.Scanner;
+
+public class TestaNumero {
+  public static void main(String[] args) {
+    Numero n = new Numero ();
+
+    boolean continuar = true;
+
+    Scanner scan = new Scanner(System.in)
+    while(continuar) {
+double x;
+double y;
+int operacao;
+
+System.out.println(x: "Digite a operação que deseja realizar:");
+
+    }
+
+    scan.close();
+  }
+}
